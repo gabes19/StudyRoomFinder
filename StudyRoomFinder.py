0fc0132e5711875aaf0f1c037333a668654ca76a
gabes19 <xxc5sg@virginia.edu>
2025-01-18 15:42:05 -0500

    Changes to index to display available times for clark library (in progress)

--- StudyRoomFinder.py
+++ StudyRoomFinder.py
@@ -1,11 +1,13 @@
 from flask import Flask, render_template, request,redirect,url_for 
+
 import modules as mod
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     selected_location = request.args.get('selected_location')
-    return render_template('index.html', selected_location = selected_location)
+    available_times = request.args.get('available_times')
+    return render_template('index.html', selected_location = selected_location, available_times = available_times)
 
 @app.route('/about')
 def about():
@@ -15,9 +17,13 @@ def about():
 @app.route("/clark")
 def clark():
     return render_template('clark.html')
-
+#TODO:
 @app.route('/show_availability', methods=['POST'])
 def show_availability():
     location = request.form.get('location')
-    return redirect(url_for('index', selected_location=location))
+    available_times = None
+    if location == "Clark Library":
+        clark = mod.collect_clark()
+        available_times = clark.rooms
+    return redirect(url_for('index', selected_location=location, available_times = available_times))
 
