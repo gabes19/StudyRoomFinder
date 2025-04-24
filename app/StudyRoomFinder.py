7bd8787a9e3407c076f6671a0a75e2d6b6351737
gabes19 <xxc5sg@virginia.edu>
2025-04-24 12:02:27 -0400

    populating the dropdown

--- app/StudyRoomFinder.py
+++ app/StudyRoomFinder.py
@@ -1,20 +1,37 @@
 from flask import Flask, render_template, request,redirect,url_for
+from models import Library, RoomAvailabilitySnapshot, Room
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, scoped_session
+from datetime import datetime, timedelta
 from config import Config 
 
+#App setup
 app = Flask(__name__)
 app.config.from_object(Config)
 
+#SQLAlchemy setup
+DATABASE_URL = app.config["SQL_ALCHEMY_DATABASE_URI"]
+engine = create_engine(DATABASE_URL, echo=False)
+SessionFactory = sessionmaker(bind=engine)
+db_session = scoped_session(SessionFactory)
+
 @app.route('/')
 def index():
-    selected_location = request.args.get('selected_location')
-    available_times = request.args.get('available_times')
-    return render_template('index.html', selected_location = selected_location, available_times = available_times)
+    libraries = db_session.query(Library).order_by(Library.library_name).all()
+    return render_template('index.html', libraries=libraries, selected_location=None, available_times=None)
+
 
 @app.route('/about')
 def about():
     return render_template('about.html')
-#TODO: Reimplement all the logic
 @app.route('/show_availability', methods=['POST'])
 def show_availability():
     pass
+
+
+
+#Session cleanup
+@app.teardown_appcontext
+def shutdown_session(exception=None):
+    db_session.remove()
 
